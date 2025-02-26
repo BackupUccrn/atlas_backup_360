2018921dd00a973609ffc752d4bf72bbd7cc9958
yassdaouk <ydaouk@ciesin.columbia.edu>
2025-02-26 13:01:16 -0500

    Update popRenderer.js

--- renderers/popRenderer.js
+++ renderers/popRenderer.js
@@ -1,16 +1,12 @@
 import RasterStretchRenderer from "@arcgis/core/renderers/RasterStretchRenderer";
 
 export const popRenderer = new RasterStretchRenderer({
-    stretchType: "standard-deviation", // Stretching with standard deviation
-    numberOfStandardDeviations: 2, // 2 standard deviations
-    gamma: [1], // Gamma correction
-
-    // Color ramp from white (transparent) to orange
-        colorRamp: {
+    stretchType: "standard-deviation",
+    numberOfStandardDeviations: 2,
+    gamma: [1],
+    colorRamp: {
         type: "algorithmic",
-        fromColor: [255, 255, 255, 0],  // White transparent (NoData)
-        toColor: [255, 140, 0, 255]  // Orange
+        fromColor: [255, 255, 255, 0],  // Transparent for NoData
+        toColor: [255, 140, 0, 255]     // Orange ramp
     }
 });
-
-
